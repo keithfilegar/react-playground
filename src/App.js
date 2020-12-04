3a9dd4747d11857d017c5093621200e3b5a72a1c
keithfilegar <keith.filegar@gmail.com>
2020-12-04 12:48:37 -0800

    upload react playground to github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import Split from './composition/Split';
 import './App.css';
+import Tooltip from './composition/Tooltip'
+
+const firstTooltip = (
+  <Tooltip color='hotpink' message='tooltip message'>
+   ipsum
+  </Tooltip>
+)
+const secondTooltip = (
+  <Tooltip color='#126BCC' message='another tooltip message'>
+    officiis
+  </Tooltip>
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='App'>
+      <Split className='left' flexBasis={2}>
+        This is the content for the left `Split`. Lorem {firstTooltip} ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
+        <Tooltip message='one more tooltip message'>
+          Necessitatibus?
+        </Tooltip>
+      </Split>
+      <Split className='right' flexBasis={5}>
+  This is the content for the right `Split`. {secondTooltip}Inventore aliquid cupiditate suscipit repellat. Quaerat quis officiis quam fuga. Aliquid quo possimus id soluta aspernatur.
+      </Split>
+    </main>
   );
 }
 
